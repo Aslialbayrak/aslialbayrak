88329e706262910194998d9609fd0d68ac95c53d
Aslialbayrak <aslik2827@gmail.com>
2020-03-25 23:31:07 +0300

    first

---
+++ src/itemDetail.js
@@ -0,0 +1,23 @@
+import React, {useState,useEffect} from 'react';
+import './App.css';
+import { Link } from 'react-router-dom'
+
+function Item() {
+    useEffect(()=> {
+        
+    },[]);
+   
+
+    const [item,setItem] =useState({});
+
+    
+
+
+    return (
+        <div>
+            <h1>item</h1>
+        </div>
+    );
+}
+
+export default Item;
